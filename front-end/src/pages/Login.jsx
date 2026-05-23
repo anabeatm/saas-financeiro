dd6c69a426d1d1fc8f3a2ab95900698b39c2fe9d
Ana Malaquias <ana.beat20051@gmail.com>
2026-05-23 13:38:06 -0300

    refactor: Login, Recover, and Register pages to use AuthLayout and Button components

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,66 +1,46 @@
 import { Link } from "react-router-dom";
 import { CoinsIcon } from "lucide-react";
 import Input from "../components/Input";
+import Button from "../components/Button";
+import AuthLayout from "../components/AuthLayout";
 
 export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#fbfaf9] p-4 font-sans">
-      <div className="w-full max-w-116">
-        <div className="mb-6 text-center">
-          <CoinsIcon className="mx-auto mb-2 h-12 w-12 text-[#a88d6f]" />
-          <h1 className="text-3xl font-extrabold text-[#a88d6f] ">FinEXP</h1>
-          <p className="mt-2 text-sm text-gray-500">
-            Your finances in the gamification world
-          </p>
+    <AuthLayout title="Login" subtitle="Welcome back to FinEXP" showLogo={true}>
+      <form className="flex flex-col gap-4">
+        <Input
+          label="E-mail"
+          id="email"
+          type="email"
+          placeholder="example@email.com"
+          required
+        />
+        <div className="flex flex-col gap-1">
+          <Input
+            label="Password"
+            id="password"
+            type="password"
+            placeholder="••••••••"
+            required
+          />
+          <Link
+            to="/recover"
+            className="self-end text-xs font-bold text-[#a88d6f] hover:text-[#d6bfa7] hover:underline"
+          >
+            Forgot your password?
+          </Link>
         </div>
-
-        <div className="w-full rounded-2xl bg-white p-8 shadow-xl">
-          <div className="mb-6 text-center">
-            <h2 className="text-xl font-bold text-[#0c0c0c]">Login</h2>
-            <p className="text-sm text-gray-500 mt-2">Welcome back to FinEXP</p>
-          </div>
-
-          <form className="flex flex-col gap-4">
-            <Input
-              label="E-mail"
-              id="email"
-              type="email"
-              placeholder="example@email.com"
-              required
-            />
-            <div className="flex flex-col gap-1">
-              <Input
-                label="Password"
-                id="password"
-                type="password"
-                placeholder="••••••••"
-                required
-              />
-              <Link
-                to="/recover"
-                className="self-end text-xs font-bold text-[#a88d6f] hover:text-[#d6bfa7] hover:underline"
-              >
-                Forgot your password?
-              </Link>
-            </div>
-            <button
-              type="submit"
-              className="mt-4 w-full rounded-lg bg-[#a88d6f] px-4 py-2 font-semibold text-white transition-colors hover:bg-[#d6bfa7] focus:outline-none focus:ring-2 focus:ring-[#cb9d6c] focus:ring-offset-2 cursor-pointer"
-            >
-              Enter
-            </button>
-            <div className="flex mt-2 text-sm gap-2 justify-center items-center">
-              <p className="text-gray-500">Doesn't have an account?</p>
-              <Link
-                to="/register"
-                className="font-semibold text-[#a88d6f] hover:text-[#d6bfa7] hover:underline"
-              >
-                Register right now
-              </Link>
-            </div>
-          </form>
+        <Button type="submit">Enter</Button>
+        <div className="flex mt-2 text-sm gap-2 justify-center items-center">
+          <p className="text-gray-500">Doesn't have an account?</p>
+          <Link
+            to="/register"
+            className="font-semibold text-[#a88d6f] hover:text-[#d6bfa7] hover:underline"
+          >
+            Register right now
+          </Link>
         </div>
-      </div>
-    </div>
+      </form>
+    </AuthLayout>
   );
 }
--- front-end/src/pages/Recover.jsx
+++ front-end/src/pages/Recover.jsx
@@ -1,42 +1,32 @@
 import { Link } from "react-router-dom";
 import Input from "../components/Input";
+import Button from "../components/Button";
+import AuthLayout from "../components/AuthLayout";
 import { ArrowLeft } from "lucide-react";
 
 export default function Recover() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#fbfaf9] p-4 font-sans">
-      <div className="w-full max-w-116 rounded-2xl bg-white p-8 shadow-xl ">
-        <div className="mb-6 text-center">
-          <h2 className="text-xl font-bold text-[#0c0c0c] text-center">
-            Recover your password
-          </h2>
-          <p className="text-sm mt-2 text-gray-500">
-            Inform your email to receive instructions
-          </p>
+    <AuthLayout
+      title="Recover your password"
+      subtitle="Inform your email to receive instructions"
+      showLogo={true}
+    >
+      <form className="flex flex-col gap-4">
+        <Input
+          label="E-mail"
+          id="email"
+          type="email"
+          placeholder="example@email.com"
+          required
+        />
+        <Button type="submit">Send</Button>
+        <div className="mt-2 flex justify-center gap-2 text-sm text-[#a88d6f] hover:text-[#d6bfa7] hover:underline">
+          <Link to="/login" className="flex gap-1 items-center font-semibold">
+            <ArrowLeft />
+            Return to login
+          </Link>
         </div>
-
-        <form className="flex flex-col gap-4">
-          <Input
-            label="E-mail"
-            id="email"
-            type="email"
-            placeholder="example@email.com"
-            required
-          />
-          <button
-            type="submit"
-            className="mt-4 w-full bg-[#a88d6f] text-white py-2 rounded-lg font-semibold hover:bg-[#d6bfa7] cursor-pointer"
-          >
-            Send
-          </button>
-          <div className="mt-2 flex justify-center gap-2 text-sm text-[#a88d6f] hover:text-[#d6bfa7] hover:underline">
-            <Link to="/login" className="flex gap-1 items-center font-semibold">
-              <ArrowLeft />
-              Return to login
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
+      </form>
+    </AuthLayout>
   );
 }
