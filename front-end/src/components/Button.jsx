e0d077207756cd9049476e7e8726b6b493fdfac1
Ana Malaquias <ana.beat20051@gmail.com>
2026-06-07 09:55:23 -0300

    fix: update Button component to center content within the button

--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, ...props }) {
   return (
     <button
       {...props}
-      className="mt-4 w-full cursor-pointer rounded-lg bg-[#a88d6f] px-4 py-2 font-semibold text-white transition-colors hover:bg-[#d6bfa7] focus:outline-none focus:ring-2 focus:ring-[#cb9d6c] focus:ring-offset-2"
+      className="mt-4 w-full flex items-center justify-center cursor-pointer rounded-lg bg-[#a88d6f] px-4 py-2 font-semibold text-white transition-colors hover:bg-[#d6bfa7] focus:outline-none focus:ring-2 focus:ring-[#cb9d6c] focus:ring-offset-2"
     >
       {children}
     </button>
