7ac049d08658eb15f3d2536042507dbdb669e029
Ana Malaquias <ana.beat20051@gmail.com>
2026-06-08 21:13:49 -0300

    feat: initialize back-end project with Maven setup and Spring Boot application structure

---
+++ back-end/src/test/java/com/saas_financeiro/finEXP/FinExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas_financeiro.finEXP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
