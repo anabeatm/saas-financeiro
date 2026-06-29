8de83db7c80a2f45f8f06912fedb8a1da73cd747
Ana Malaquias <ana.beat20051@gmail.com>
2026-06-29 20:48:21 -0300

    chore: stop tracking .env file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import process from "node:process";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+    proxy: {
+      "/auth": "http://localhost:8080/",
+    },
+  },
 });
