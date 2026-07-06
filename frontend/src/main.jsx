eb08e27e1fc39cddafdc58c08587d58ac089b6b6
Ana Malaquias <ana.beat20051@gmail.com>
2026-07-06 07:27:19 -0300

    refactor: reorder import statements in main.jsx for consistency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
-import Login from "./pages/Login.jsx";
 import { ToastProvider } from "./components/ToastContext.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
