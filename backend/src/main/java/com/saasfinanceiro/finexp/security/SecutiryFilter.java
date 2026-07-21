0c0d79b997dfd1dad1d1209d4272171e9ba3302e
Ana Malaquias <ana.beat20051@gmail.com>
2026-07-21 20:40:15 -0300

    feat: add SecutiryFilter for JWT token validation and user authentication

---
+++ backend/src/main/java/com/saasfinanceiro/finexp/security/SecutiryFilter.java
@@ -0,0 +1,59 @@
+package com.saasfinanceiro.finexp.security;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.saasfinanceiro.finexp.model.User;
+import com.saasfinanceiro.finexp.repository.UserRepository;
+
+@Component
+public class SecutiryFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    private SecutiryFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService=tokenService;
+        this.userRepository=userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var login = tokenService.validateToken(token);
+
+            if (!login.isEmpty()) {
+                Optional<User> userOptional = userRepository.findByEmail(login);
+
+                if (userOptional.isPresent()) {
+                    User user = userOptional.get();
+
+                    var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
